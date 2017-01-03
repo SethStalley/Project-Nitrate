87b2bfd83257ae1bf2b4cd16838f44e975e81a35
bloed <josuearrietasalas@gmail.com>
2017-01-03 16:42:24 -0600

    DB: Modifications on Users
    - added update for users.
    - added: completeName, telephoneNumber and email.

--- MolabsDB/DML/StoredProcedures/SELECTS/selectAllUsersForAdministrator.sql
+++ MolabsDB/DML/StoredProcedures/SELECTS/selectAllUsersForAdministrator.sql
@@ -13,7 +13,7 @@ BEGIN
 		SET MESSAGE_TEXT = 'Validación de usuario incorrecta.';
 	END IF;
     
-    SELECT idUser, userName, type, date
+    SELECT idUser, userName, type, date,completeName, telephoneNumber, email
 		FROM molabsdb.users
 			WHERE type = 'user' AND createdBy = pUserName;
 		
--- MolabsDB/testData.sql
+++ MolabsDB/testData.sql
@@ -1,11 +1,20 @@
-INSERT INTO molabsdb.users(username, password, type, date)
-	VALUES('root' , CAST(SHA2('root', 512) AS BINARY), 'owner', NOW());
+INSERT INTO molabsdb.users(username, password, type, date, completeName, telephoneNumber, email)
+	VALUES('root' , CAST(SHA2('root', 512) AS BINARY), 'owner', NOW(), 'Root Quiros', '6666-6666','root@gmail.com');
 
-CALL molabsdb.insertUser('seth', CAST(SHA2('omg', 512) AS BINARY), 'user','root' , CAST(SHA2('root', 512) AS BINARY));
-CALL molabsdb.insertUser('adrian', CAST(SHA2('lopez', 512) AS BINARY), 'admin','root' , CAST(SHA2('root', 512) AS BINARY));
-CALL molabsdb.insertUser('admin', CAST(SHA2('admin', 512) AS BINARY), 'admin','root' , CAST(SHA2('root', 512) AS BINARY));
-CALL molabsdb.insertUser('josue', CAST(SHA2('caca', 512) AS BINARY), 'user','root' , CAST(SHA2('root', 512) AS BINARY));
-CALL molabsdb.insertUser('user', CAST(SHA2('user', 512) AS BINARY), 'user','adrian' , CAST(SHA2('lopez', 512) AS BINARY));
+CALL molabsdb.insertUser('seth', CAST(SHA2('omg', 512) AS BINARY), 'user', 'Seth Stalley', '1234-5678','seth@hotmail.com',
+	'root' , CAST(SHA2('root', 512) AS BINARY));
+    
+CALL molabsdb.insertUser('adrian', CAST(SHA2('lopez', 512) AS BINARY), 'admin', 'Adriàn Lòpez', '8765-4321','adrian@hotmail.com',
+	'root' , CAST(SHA2('root', 512) AS BINARY));
+    
+CALL molabsdb.insertUser('admin', CAST(SHA2('admin', 512) AS BINARY),  'admin', 'Admin Admin', '2222-2222','admin@hotmail.com',
+	'root' , CAST(SHA2('root', 512) AS BINARY));
+    
+CALL molabsdb.insertUser('josue', CAST(SHA2('caca', 512) AS BINARY),  'user','Josue Arrieta Salas', '8327-3913','josuearrietasalas@gmail.com',
+	'root' , CAST(SHA2('root', 512) AS BINARY));
+
+CALL molabsdb.insertUser('user', CAST(SHA2('user', 512) AS BINARY),  'user','User User', '1111-1111','user@hotmail.com',
+	'adrian' , CAST(SHA2('lopez', 512) AS BINARY));
 
 SELECT * FROM molabsdb.users;
 SELECT * FROM molabsdb.graphs;
