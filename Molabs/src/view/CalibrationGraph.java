6aa3251bbb31e3869ef284b154042cb9af824cdf
xelop <adrianlq8@gmail.com>
2017-01-13 21:02:43 -0600

    file extension, only images

--- Molabs/src/view/CalibrationGraph.java
+++ Molabs/src/view/CalibrationGraph.java
@@ -4,6 +4,8 @@ import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
@@ -16,8 +18,12 @@ import java.io.FileOutputStream;
 import java.io.IOException; 
 import java.util.Random;
 
+import javax.swing.JFileChooser;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import com.google.gson.internal.bind.util.ISO8601Utils;
 
@@ -37,6 +43,7 @@ import de.erichseifert.gral.plots.points.PointRenderer;
 import de.erichseifert.gral.ui.InteractivePanel;
 import de.erichseifert.gral.util.GraphicsUtils;
 import model.Calibration;
+import values.OptionErrorMessage;
 import values.Strings;
 
 
@@ -45,8 +52,8 @@ public class CalibrationGraph extends JPanel{
 	public CalibrationGraph(Calibration calibration) {
 		// receives (x,y)  (concentration, absorbance)
 		super(new BorderLayout());
-		setPreferredSize(new Dimension(400, 200));
-		setBackground(Color.WHITE);
+		setPreferredSize(new Dimension(48, 245));
+		setBackground(Color.BLUE);
 		
 		// Generate data
 		DataTable data = new DataTable(Double.class, Double.class);
@@ -73,6 +80,7 @@ public class CalibrationGraph extends JPanel{
 		// Create and format upper plot
 		XYPlot plot = new XYPlot(data,data2);
 		plot.setInsets(new Insets2D.Double(10.0, 65.0, 60.0, 30.0));
+		plot.setBackground(Color.WHITE);
 		
 		if (calibration != null){
 
@@ -86,17 +94,27 @@ public class CalibrationGraph extends JPanel{
 			plot.setPointRenderers(data, null);
 			plot.setPointRenderers(data2, null);
 		}
-		
 
 		
 		// axis
 		plot.getAxisRenderer(XYPlot.AXIS_X).getLabel().setText("Concentration (mg/L)");
 		plot.getAxisRenderer(XYPlot.AXIS_Y).getLabel().setText("ABS (U.A.)");
-		
-
+	
 
 		InteractivePanel panel = new InteractivePanel(plot);
-		
+		panel.setBackground(Color.BLACK);
+		JPopupMenu pop = new JPopupMenu();
+		JMenuItem item = new JMenuItem("Export Graph");
+		item.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				save(plot);
+				
+			}
+		});
+		pop.add(item);
+		panel.setComponentPopupMenu(pop);
 		add(panel);
 	}
 	
@@ -118,6 +136,46 @@ public class CalibrationGraph extends JPanel{
 		
 		Double[] result = {xMin + (xMin * Strings.EXTRA_PORCENTAGE), xMax + (xMax * Strings.EXTRA_PORCENTAGE)};
 		return result;
+	}
+	
+	private void save(Drawable plot){
+		JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter1 = new FileNameExtensionFilter("PNG", "png");
+        FileNameExtensionFilter filter2 = new FileNameExtensionFilter("JPG", "JPG");
+        chooser.addChoosableFileFilter(filter1);
+        chooser.addChoosableFileFilter(filter2);
+        chooser.setDialogTitle("Export Image");
+        chooser.setAcceptAllFileFilterUsed(false);
+        int option = chooser.showSaveDialog(null);
+        
+		if(option == JFileChooser.APPROVE_OPTION){
+			File file = chooser.getSelectedFile();
+			try{
+				DrawableWriter writer;
+				if(chooser.getFileFilter().equals(filter2)){
+					writer = DrawableWriterFactory.getInstance().get("image/jpeg");
+					if (!file.toString().contains(".jpg") )
+						file = new File(file.getAbsolutePath().concat(".jpg"));
+				}else{
+					writer = DrawableWriterFactory.getInstance().get("image/png");
+					if (!file.toString().contains(".png") )
+						file = new File(file.getAbsolutePath().concat(".png"));
+				}
+				int result = JOptionPane.YES_OPTION;
+				if(file.exists())
+					result = JOptionPane.showOptionDialog(null, "File already exists, overwrite?", "Warning",
+							JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+							null, OptionErrorMessage.YES_NO_ENGLISH, OptionErrorMessage.YES_NO_ENGLISH[0]);
+				
+				if(result == JOptionPane.YES_OPTION){
+					FileOutputStream fileStream = new FileOutputStream(file);
+					writer.write(plot,fileStream, plot.getWidth(), plot.getHeight());
+					fileStream.close();
+				}
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+		}
 	}
 
 
